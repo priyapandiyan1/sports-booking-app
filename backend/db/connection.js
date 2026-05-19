a9fe95a6d805720dd1504202c63c15b6cf1d53a3
priyapandiyan1 <priyapandiyan62@gmail.com>
2026-05-19 16:41:22 +0530

    backend update

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,21 +1,18 @@
+const sqlite3 = require('sqlite3').verbose();
+const { open } = require('sqlite');
+const path = require('path');
 
-const sqlite3 = require("sqlite3").verbose();
-const { open } = require("sqlite");
-const path = require("path");
-
-let dbPromise;
+let db;
 
 async function getDb() {
-  if (!dbPromise) {
-    dbPromise = open({
-      filename: path.join(__dirname, "database.sqlite"),
+  if (!db) {
+    db = await open({
+      filename: path.join(__dirname, 'database.sqlite'),
       driver: sqlite3.Database,
-    }).then(db => {
-      console.log("Database connected");
-      return db;
     });
+    console.log('Database connected');
   }
-  return dbPromise;
+  return db;
 }
 
 module.exports = getDb;
